91c97c4e3e7d90e75c0110648135b4ba25475458
omprakash1305 <ombshss2005@gmail.com>
2025-08-16 14:48:19 +0530

    new

---
+++ CourseManagementModule/src/main/java/com/tns/course/CourseManagementModuleApplication.java
@@ -0,0 +1,15 @@
+//Driver program to start the Course Management Module application.
+package com.tns.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementModuleApplication {
+
+	//Main method to start the Spring Boot application.
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementModuleApplication.class, args);
+	}
+
+}
